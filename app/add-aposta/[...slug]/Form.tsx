e4bc9b95232b567f279cf453a29fdb512152a64b
alexandreoliv <alexandrebo@gmail.com>
2023-09-30 03:38:28 +0200

    add add-aposta page and add-aposta API endpoint

---
+++ app/add-aposta/[...slug]/Form.tsx
@@ -0,0 +1,190 @@
+"use client";
+
+import {
+	Button,
+	Container,
+	FormControl,
+	InputLabel,
+	MenuItem,
+	Select,
+	TextField,
+	Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+
+export const sendAposta = async (obj: {
+	ano: number;
+	serie: string;
+	nome: string;
+	aposta: number[];
+}) => {
+	const response = await fetch(`http://localhost:3000/api/add-aposta/`, {
+		method: "POST",
+		body: JSON.stringify(obj),
+	});
+
+	// TODO: add error handling
+	return;
+};
+
+const Form = ({
+	equipes,
+	ano,
+	serie,
+}: {
+	equipes: string[];
+	ano: number;
+	serie: string;
+}) => {
+	const [posicoes, setPosicoes] = useState<
+		{ equipe: string; posicao: number }[]
+	>([]);
+	const [numeros, setNumeros] = useState([
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+	]);
+	const [usedNumeros, setUsedNumeros] = useState<number[]>([]);
+	const [nome, setNome] = useState("");
+	const [disabled, setDisabled] = useState(false);
+
+	useEffect(() => {
+		if (equipes) {
+			const newPosicoes = equipes.map((e) => ({
+				equipe: e,
+				posicao: 0,
+			}));
+			setPosicoes(newPosicoes);
+		}
+	}, [equipes]);
+
+	const onPosicaoChange = (equipe: string, posicao: number) => {
+		// gets previous position of this team
+		const previousPosition =
+			posicoes.find((p) => p.equipe === equipe)?.posicao || 0;
+
+		// adds the new position in the array of used positions
+		const usedNumerosNew = [...usedNumeros];
+		usedNumerosNew.push(posicao);
+
+		// removes the previous position from the array of used positions
+		if (previousPosition) {
+			const indexToRemove = usedNumerosNew.indexOf(previousPosition);
+			if (indexToRemove !== -1) {
+				usedNumerosNew.splice(indexToRemove, 1);
+			}
+		}
+
+		// now updates Posicoes
+		const posicoesNew = posicoes.map((p) =>
+			p.equipe === equipe ? { equipe, posicao } : p
+		);
+
+		setUsedNumeros(usedNumerosNew);
+		setPosicoes(posicoesNew);
+	};
+
+	const onFinish = async () => {
+		try {
+			const aposta = equipes.map(
+				(e) => posicoes.find((p) => p.equipe === e)?.posicao!
+			);
+
+			const obj = { ano, serie, nome, aposta };
+			setDisabled(true);
+
+			const response = await sendAposta(obj);
+			console.log({ response });
+		} catch (error) {
+			console.error("Failed to submit the form", error);
+		}
+	};
+
+	if (!equipes) {
+		console.log("no props yet, will return null");
+		return null; // no props yet
+	}
+
+	return (
+		<Container>
+			<Typography
+				variant="h4"
+				style={{
+					fontWeight: "bold",
+					margin: "0 0 10px 10px",
+					textAlign: "center",
+				}}
+			>
+				Adicionar Aposta
+			</Typography>
+
+			<form name="basic" onSubmit={onFinish} autoComplete="off">
+				<TextField
+					label="Nome"
+					name="nome"
+					value={nome}
+					onChange={(e) => setNome(e.target.value)}
+					variant="outlined"
+					fullWidth
+					required
+					margin="normal"
+					InputLabelProps={{
+						shrink: true,
+					}}
+				/>
+
+				{equipes.map((e) => {
+					const teamPosicao =
+						posicoes.find((p) => p.equipe === e)?.posicao || "";
+
+					return (
+						<FormControl
+							fullWidth
+							variant="outlined"
+							margin="normal"
+							key={e}
+							required
+						>
+							<InputLabel>{e}</InputLabel>
+							<Select
+								label={e}
+								value={String(teamPosicao)}
+								defaultValue=""
+								renderValue={(value) => (value ? value : "")}
+								onChange={(event) =>
+									onPosicaoChange(
+										e,
+										parseInt(event.target.value)
+									)
+								}
+							>
+								{numeros.map((p) => (
+									<MenuItem
+										value={p}
+										key={p}
+										disabled={
+											usedNumeros.find((n) => n === p) ===
+											p
+										}
+									>
+										{p}
+									</MenuItem>
+								))}
+							</Select>
+						</FormControl>
+					);
+				})}
+
+				<Button
+					type="submit"
+					variant="contained"
+					color="primary"
+					disabled={disabled}
+					fullWidth
+				>
+					{disabled ? "Aposta enviada" : "Enviar"}
+				</Button>
+			</form>
+		</Container>
+	);
+};
+
+export default Form;
---
+++ app/api/add-aposta/route.ts
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/prisma/client";
+import { validateAnoSerieSlug } from "@/utils/validateAnoSerieSlug";
+
+const validateAposta = (body: {
+	ano: number;
+	serie: string;
+	nome: string;
+	aposta: number[];
+}) => {
+	if (!body?.aposta || !body?.nome || !body?.ano || !body?.serie)
+		return false;
+	const { aposta, nome, ano, serie } = body;
+
+	if (ano !== 2023) return false;
+
+	if (aposta?.length !== 20) return false;
+
+	if (typeof nome !== "string" || typeof serie !== "string") return false;
+
+	if (serie.toUpperCase() !== "A" && serie.toUpperCase() !== "B")
+		return false;
+
+	return true;
+};
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+
+	const isValidAposta = validateAposta(body);
+	if (!isValidAposta) {
+		return NextResponse.json({ error: "Dados inválidos" }, { status: 404 });
+	}
+
+	const { aposta, nome, ano } = body;
+	const serie = body.serie.toUpperCase();
+
+	const response = await prisma.apostas.create({
+		data: {
+			nome,
+			serie,
+			ano,
+			aposta,
+			v: 0,
+		},
+	});
+
+	return NextResponse.json(response);
+}
